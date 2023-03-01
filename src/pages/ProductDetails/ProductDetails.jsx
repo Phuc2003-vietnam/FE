e6eab3a71c68ca24718d6cbfbbf7f52fa92773fa
Your Name <you@example.com>
2023-03-01 17:42:45 +0700

    add ProductDetails

---
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProductDetails = props => {
+  return (
+    <div>ProductDetails</div>
+  )
+}
+
+ProductDetails.propTypes = {}
+
+export default ProductDetails
---
+++ src/pages/ProductDetails/ProductList/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProductList = props => {
+  return (
+    <div>ProductList</div>
+  )
+}
+
+ProductList.propTypes = {}
+
+export default ProductList
